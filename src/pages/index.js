b4a0bf56e70ee020e8dc870456b61c48f933136e
NtshVrm <nitishvarma2k@gmail.com>
2022-03-22 01:49:30 +0530

    feat : basic routing setup

---
+++ src/pages/index.js
@@ -0,0 +1,6 @@
+import Home from "./Home/Home";
+import Wishlist from "./Wishlist/Wishlist";
+import Cart from "./Cart/Cart";
+import ProductListing from "./ProductListing/ProductListing";
+
+export { Home, Wishlist, Cart, ProductListing };
