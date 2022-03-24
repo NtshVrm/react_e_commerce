b9ff96eb8caf0c28e6b0290d9ec2013fa96a28d2
NtshVrm <nitishvarma2k@gmail.com>
2022-03-24 22:48:35 +0530

    feat : updated category component

--- src/backend/db/categories.js
+++ src/backend/db/categories.js
@@ -1,5 +1,5 @@
 import { v4 as uuid } from "uuid";
-
+import { f1_hoodie, cap, shirt } from "../../assets";
 /**
  * Category Database can be added here.
  * You can add category of your wish with different attributes
@@ -9,26 +9,31 @@ export const categories = [
   {
     _id: uuid(),
     categoryName: "Hoodies",
+    image: f1_hoodie,
     display: "large",
   },
   {
     _id: uuid(),
     categoryName: "Caps",
+    image: cap,
     display: "small",
   },
   {
     _id: uuid(),
     categoryName: "T-shirts",
+    image: shirt,
     display: "small",
   },
   {
     _id: uuid(),
     categoryName: "Bags",
+    image: "",
     display: "small",
   },
   {
     _id: uuid(),
     categoryName: "Helmets",
+    image: "",
     display: "small",
   },
 ];
--- src/components/home/Category.jsx
+++ src/components/home/Category.jsx
@@ -1,48 +1,74 @@
 import { Link } from "react-router-dom";
-import { useEffect } from "react/cjs/react.production.min";
+import { useEffect, useState } from "react";
 import { f1_hoodie, cap, shirt } from "../../assets";
 import axios from "axios";
 
 export function Category(){
 
+    const [categories, setCategories] = useState([]);
+
+    useEffect(() => {
+        async function getCat(){
+            try{
+                const response = await axios.get("/api/categories");
+                setCategories(response.data.categories)
+            }
+            catch(error){
+                console.log("Could not fetch the categories", error);
+            }
+            
+        }
+        getCat();
+    },[])
+
+    const outputCat = categories.slice(0,3);
+    const itemLarge = outputCat.filter((item) => item.display==="large");
+    const itemSmall = outputCat.filter((item) => item.display==="small");
+
     return(
         <div className="category-container">
-                    <div className="category-header">
-                        <div className="category-title">Shop by category</div>
-                        <Link className="category-action" to="/ProductListing">
-                            Browse all categories<i className="fas fa-arrow-right"></i>
-                        </Link>
-                    </div>
 
-                    <div className="category-list">
-                        <div className="item-large">
-                            <div className="item">
-                                <img
-                                    className="item-img-large"
-                                    src={f1_hoodie}
-                                    alt="cat-large-img" />
-                                <Link className="item-title" to="/ProductListing">Hoodies</Link>
-                            </div>
-                        </div>
+            <div className="category-header">
+                <div className="category-title">Shop by category</div>
+                <Link className="category-action" to="/ProductListing">
+                    Browse all categories<i className="fas fa-arrow-right"></i>
+                </Link>
+            </div>
 
-                        <div className="item-small">
-                            <div className="item">
-                                <img
-                                    className="item-img-small"
-                                    src={cap}
-                                    alt="" />
-                                <Link className="item-title" to="/ProductListing">Caps</Link>
-                            </div>
+            <div className="category-list">    
 
-                            <div className="item">
-                                <img
-                                    className="item-img-small"
-                                    src={shirt}
-                                    alt="" />
-                                <Link className="item-title" to="/ProductListing">T-Shirts</Link>
-                            </div>
-                        </div>
-                    </div>
+                <div className="item-large">
+                    {
+                        itemLarge && itemLarge.map((item) => {
+                            return(
+                                <div key={item.id} className="item">
+                                    <img
+                                        className="item-img-large"
+                                        src={item.image}
+                                        alt="cat-large-img" />
+                                    <Link className="item-title" to="/ProductListing">{item.categoryName}</Link>
+                                </div>
+                            )
+                        })
+                    }
                 </div>
+
+                <div className="item-small">
+                    {
+                        itemSmall && itemSmall.map((item) => {
+                            return(
+                                <div key={item.id} className="item">
+                                    <img
+                                        className="item-img-small"
+                                        src={item.image}
+                                        alt="cat-large-small" />
+                                    <Link className="item-title" to="/ProductListing">{item.categoryName}</Link>
+                                </div>
+                            )
+                        })
+                    }
+                </div>
+            </div>
+        </div>
     );
 }
